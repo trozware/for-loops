7cfcd2ced5f8c691a0bb675325fa1da6766afa7f
Sarah Reichelt <sarah@troz.net>
2016-03-16 16:55:35 +1000

    Add playground

---
+++ For-Loops.playground/Contents.swift
@@ -0,0 +1,97 @@
+// : # For Loops:
+//: [Playground to accompany this tutorial article](http://troz.net/2016/03/learning-swift-loops/)
+//:
+
+//: ## Looping through a range:
+//: Half-open range:
+var result: [Int] = []
+for x in 0 ..< 5 {
+    result.append(x)
+}
+result
+
+//: Closed range:
+result = []
+for x in 0 ... 5 {
+    result.append(x)
+}
+result
+
+//: ## Striding through a range:
+//: stride(to:by:):
+result = []
+for x in 0.stride(to: 10, by: 2) {
+    result.append(x)
+}
+result
+
+//: stride(through:by:):
+result = []
+for x in 0.stride(through: 10, by: 2) {
+    result.append(x)
+}
+result
+
+//: stride by a negative amount:
+result = []
+for x in 10.stride(through: 0, by: -2) {
+    result.append(x)
+}
+result
+
+//: stride with floating point numbers:
+var resultDouble: [Double] = []
+let startingNumber = 0.1
+for x in startingNumber.stride(through: 1.0, by: 0.2) {
+    resultDouble.append(x)
+}
+resultDouble
+
+//: Note that startingNumber has not changed.
+startingNumber
+
+//: ## Looping through a range using where:
+result = []
+for x in 0 ... 10 where x % 3 == 0 {
+    result.append(x)
+}
+result
+
+//: ## Looping through an array:
+let arrayOfInts = [1, 2, 3, 4, 5, 6]
+result = []
+for x in arrayOfInts {
+    result.append(x * 2)
+}
+result
+
+//: ## Looping through a dictionary:
+let myDict = [1: "one", 2: "two", 3: "three"]
+var resultString: [String] = []
+for (key, value) in myDict {
+    resultString.append("\(key) = \(value)")
+}
+resultString
+
+//: ## Looping through an array using enumerate():
+let arrayOfNames = ["Andy", "Buzz", "Woody"]
+resultString = []
+for (index, element) in arrayOfNames.enumerate() {
+    resultString.append("The name at index \(index) is \(element)")
+}
+resultString
+
+//: ## Looping through an array of optionals:
+let arrayWithOptionals: [String?] = ["Woody", nil, "Buzz", nil, "Rex"]
+resultString = []
+for case let name? in arrayWithOptionals {
+    resultString.append(name)
+}
+resultString
+
+//: ## Looping through an array with where:
+resultString = []
+for case let name? in arrayWithOptionals where name.characters.count < 5 {
+    resultString.append(name)
+}
+resultString
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # for-loops
+
 Playground for Learning Swift - For-Loops
+
+http://troz.net/2016/03/learning-swift-loops/
